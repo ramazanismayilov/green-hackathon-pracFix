e44c87e22e33a0bc00d9cf846abe51a6d1cf4c2d
unknown <ismayiloff522@gmail.com>
2024-10-28 22:47:07 +0400

    expert card design

--- app/components/AllPosts.jsx
+++ app/components/AllPosts.jsx
@@ -1,7 +1,8 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { CircularProgress } from '@mui/material';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { CircularProgress } from "@mui/material";
+import PostCard from "./PostCard"; // PostCard komponentini import edirik
 
 function AllPosts() {
   const [posts, setPosts] = useState([]);
@@ -11,11 +12,13 @@ function AllPosts() {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await axios.get('https://pracfix-back.onrender.com/api/post'); // Tüm gönderileri al
+        const response = await axios.get(
+          "https://pracfix-back.onrender.com/api/post"
+        );
         setPosts(response.data);
       } catch (error) {
-        setError('Gönderiler yüklenirken bir hata oluştu.');
-        console.error('Error fetching posts:', error);
+        setError("Gönderiler yüklenirken bir hata oluştu.");
+        console.error("Error fetching posts:", error);
       } finally {
         setLoading(false);
       }
@@ -25,37 +28,30 @@ function AllPosts() {
   }, []);
 
   if (loading) {
-    return <div className="flex justify-center items-center h-screen">
-      <CircularProgress sx={{color : "green"}} />
-    </div>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <CircularProgress sx={{ color: "green" }} />
+      </div>
+    );
   }
 
   if (error) {
     return <div className="text-center text-red-500 mt-4">{error}</div>;
   }
 
-  console.log(posts);
-
   return (
-    <div className="flex flex-col min-h-[400px] mt-10">
+    <div className="flex flex-col mt-10">
       <h2 className="text-4xl text-center font-bold mb-6">Bütün Postlar</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl w-full">
         {posts.map((post) => (
-          <div key={post._id} className="bg-white shadow-md rounded-lg p-6 border border-green-500 transition-transform transform hover:scale-105 hover:shadow-lg">
-            {post.blogPhoto && (
-              <img
-                src={`https://pracfix-back.onrender.com/${post.blogPhoto}`}
-                alt={post.title}
-                className="w-full h-40 object-cover rounded-lg mb-4"
-              />
-            )}
-            <h3 className="text-xl font-semibold text-green-800 mb-2">{post.title}</h3>
-            <p className="text-green-600 mb-4 text-sm">{post.description}</p>
-
-            <a href={`/posts/${post._id}`} className="text-green-500 font-medium hover:underline">
-              Daha Çox Gör
-            </a>
-          </div>
+          <PostCard
+            key={post._id}
+            date={post.createdAt}
+            title={post.title}
+            description={post.description}
+            image={`https://pracfix-back.onrender.com/${post.blogPhoto}`}
+            id={post._id}
+          />
         ))}
       </div>
     </div>
--- app/components/PostDetails.jsx
+++ app/components/PostDetails.jsx
@@ -4,18 +4,25 @@ import axios from "axios";
 import Rating from "@mui/material/Rating";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { CircularProgress, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import {
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
 
 function PostDetails() {
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [rating, setRating] = useState(0); 
-  const [comment, setComment] = useState(""); 
-  const [reviews, setReviews] = useState([]); 
-  const [openDialog, setOpenDialog] = useState(false); 
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+  const [reviews, setReviews] = useState([]);
+  const [openDialog, setOpenDialog] = useState(false);
 
-  const userId = typeof window !== 'undefined' ? localStorage.getItem("userId") : null;
+  const userId =
+    typeof window !== "undefined" ? localStorage.getItem("userId") : null;
 
   useEffect(() => {
     const pathname = window.location.pathname;
@@ -23,7 +30,9 @@ function PostDetails() {
 
     const fetchPost = async () => {
       try {
-        const response = await axios.get(`https://pracfix-back.onrender.com/api/post/post/${id}`);
+        const response = await axios.get(
+          `https://pracfix-back.onrender.com/api/post/post/${id}`
+        );
         setPost(response.data);
       } catch (error) {
         setError("Gönderi yüklenirken bir hata oluştu.");
@@ -34,7 +43,9 @@ function PostDetails() {
 
     const fetchReviews = async () => {
       try {
-        const response = await axios.get(`https://pracfix-back.onrender.com/api/review/${id}`);
+        const response = await axios.get(
+          `https://pracfix-back.onrender.com/api/review/${id}`
+        );
         setReviews(response.data.reviews);
       } catch (error) {
         console.error("Yorumlar alınırken hata:", error);
@@ -53,13 +64,16 @@ function PostDetails() {
 
     const pathname = window.location.pathname;
     const postId = pathname.split("/").pop();
-    
+
     try {
-      const response = await axios.post(`https://pracfix-back.onrender.com/api/review/${postId}`, {
-        rating,
-        comment,
-        userId,
-      });
+      const response = await axios.post(
+        `https://pracfix-back.onrender.com/api/review/${postId}`,
+        {
+          rating,
+          comment,
+          userId,
+        }
+      );
 
       setReviews((prevReviews) => [...prevReviews, response.data.review]);
       setRating(0);
@@ -73,76 +87,106 @@ function PostDetails() {
     setOpenDialog(false);
   };
 
-  if (loading) return <div className="flex items-center min-h-[200px] justify-center"> <CircularProgress sx={{color : "green"}} /></div>;
+  if (loading)
+    return (
+      <div className="flex items-center min-h-[200px] justify-center">
+        {" "}
+        <CircularProgress sx={{ color: "green" }} />
+      </div>
+    );
   if (error) return <div>{error}</div>;
 
   return (
     <div className="flex gap-8 flex-wrap justify-between mt-10 p-5 bg-white rounded-lg shadow-lg">
-  <img className="w-full  rounded-lg shadow-md transition-transform duration-300 hover:scale-105" src={`https://pracfix-back.onrender.com/${post.blogPhoto}`} alt={post?.title} />
-
-  <div className="flex flex-col gap-5 w-full max-w-xl">
-    <h1 className="text-3xl font-bold text-gray-800 hover:text-blue-600 transition-colors duration-200">{post?.title}</h1>
-    <p className="text-gray-700 leading-relaxed">{post?.content}</p>
-    <p className="text-gray-600">{post?.description}</p>
-
-    <div className="flex flex-col gap-4">
-      <h2 className="text-xl font-semibold text-gray-800">İstifadəçi Rəyləri</h2>
-      {reviews.map((review, index) => (
-        <div key={index} className="flex gap-4 p-4 border border-gray-300 rounded-lg shadow-sm hover:shadow-lg transition-shadow duration-200">
-          <div className="flex flex-col">
-            <Rating value={review.rating} readOnly />
-            <small className="text-gray-600 font-semibold">
-              {review.user.firstName} {review.user.lastName}
-            </small>
-          </div>
-          <p className="text-gray-800">{review.comment}</p>
-        </div>
-      ))}
-    </div>
-
-    <div className="flex flex-col gap-6">
-      <h2 className="text-lg font-semibold text-gray-800">Rəy Bildir</h2>
-      <TextField
-        label="Yorumunuzu yazın..."
-        variant="outlined"
-        fullWidth
-        multiline
-        rows={4}
-        value={comment}
-        onChange={(e) => setComment(e.target.value)}
-        className="rounded-md border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200"
+      <img
+        className="w-full  rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
+        src={`https://pracfix-back.onrender.com/${post.blogPhoto}`}
+        alt={post?.title}
       />
-      <div className="flex items-center gap-3">
-        <Rating
-          name="rating"
-          value={rating}
-          onChange={(event, newValue) => {
-            setRating(newValue);
-          }}
-        />
-        <Button variant="contained" color="primary" onClick={handleReviewSubmit} className="shadow-md hover:shadow-lg transition-shadow duration-200">
-          Yorum Ekle
-        </Button>
+
+      <div className="flex flex-col gap-5 w-full max-w-xl">
+        <h1 className="text-3xl font-bold text-gray-800 hover:text-blue-600 transition-colors duration-200">
+          {post?.title}
+        </h1>
+        <p className="text-gray-700 leading-relaxed">{post?.content}</p>
+        <p className="text-gray-600">{post?.description}</p>
+
+        <div className="flex flex-col gap-4">
+          <h2 className="text-xl font-semibold text-gray-800">
+            İstifadəçi Rəyləri
+          </h2>
+          {reviews.map((review, index) => (
+            <div
+              key={index}
+              className="flex gap-4 p-4 border border-gray-300 rounded-lg shadow-sm hover:shadow-lg transition-shadow duration-200"
+            >
+              <div className="flex flex-col">
+                <Rating value={review.rating} readOnly />
+                <small className="text-gray-600 font-semibold">
+                  {review.user.firstName} {review.user.lastName}
+                </small>
+              </div>
+              <p className="text-gray-800">{review.comment}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="flex flex-col gap-6">
+          <h2 className="text-lg font-semibold text-gray-800">Rəy Bildir</h2>
+          <TextField
+            label="Yorumunuzu yazın..."
+            variant="outlined"
+            fullWidth
+            multiline
+            rows={4}
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            className="rounded-md border-gray-300 focus:border-blue-500 focus:ring focus:ring-blue-200"
+          />
+          <div className="flex items-center gap-3">
+            <Rating
+              name="rating"
+              value={rating}
+              onChange={(event, newValue) => {
+                setRating(newValue);
+              }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleReviewSubmit}
+              className="shadow-md hover:shadow-lg transition-shadow duration-200"
+            >
+              Yorum Ekle
+            </Button>
+          </div>
+        </div>
       </div>
+
+      <Dialog open={openDialog} onClose={handleCloseDialog}>
+        <DialogTitle className="text-lg font-bold">
+          Kayıt Olmanız Gerek
+        </DialogTitle>
+        <DialogContent>
+          <p className="text-gray-700">
+            Yorum yapabilmek için lütfen kaydolun.
+          </p>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="secondary">
+            Kapat
+          </Button>
+          <Button
+            onClick={() => {
+              window.location.href = "/register";
+            }}
+            color="primary"
+          >
+            Kayıt Ol
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
-  </div>
-
-  <Dialog open={openDialog} onClose={handleCloseDialog}>
-    <DialogTitle className="text-lg font-bold">Kayıt Olmanız Gerek</DialogTitle>
-    <DialogContent>
-      <p className="text-gray-700">Yorum yapabilmek için lütfen kaydolun.</p>
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={handleCloseDialog} color="secondary">
-        Kapat
-      </Button>
-      <Button onClick={() => { window.location.href = '/register'; }} color="primary">
-        Kayıt Ol
-      </Button>
-    </DialogActions>
-  </Dialog>
-</div>
-
   );
 }
 
